04f0cd46ee008dadd0a5c0d6e7dc2263ee29abb8
1873645345@qq.com <JIANG22513822>
2018-01-03 09:50:30 +0800

    整理了一些

--- phpDemo/makeqrcode.php
+++
@@ -1,9 +0,0 @@
-<?php
-//生成二维码的DEMO
-include "./phpqrcode/phpqrcode.php";
-$value="http://www.baidu.com";
-$errorCorrectionLevel = "L"; // 纠错级别：L、M、Q、H
-$matrixPointSize = "4"; // 点的大小：1到10
-$path = './qrcode.png';
-QRcode::png($value, false, $errorCorrectionLevel, $matrixPointSize);
-?>
--- phpDemo/php-mysql/mystudy/conf/config.php
+++
@@ -1,5 +0,0 @@
-<?php                                               
-mysql_connect('localhost','root','root') or die('连接数据库失败');
-mysql_select_db('test') or die(mysql_error());
-mysql_query('set names utf8');
-?>
