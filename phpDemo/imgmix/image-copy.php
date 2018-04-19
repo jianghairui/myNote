032e22ddb96f09ca06a253b9da74153d13166a94
1873645345@qq.com <JIANG22513822>
2018-04-19 11:54:34 +0800

    对图片合成进行了整理

---
+++ phpDemo/imgmix/image-copy.php
@@ -0,0 +1,50 @@
+<?php
+header('Content-Type:image/png');
+//底层头像图片
+$path_1 = "./huof.png";
+//上层指纹图片
+$path_2 = "./huo.jpg";
+
+$image_1 = imagecreatefrompng($path_1);
+$image_2 = imageresize($path_2,750,1008);
+
+//将$image_1 copy 到 $image_2中
+imagecopyresampled($image_2,$image_1,350,504,350,504,imagesx($image_1),imagesy($image_1),imagesx($image_1),imagesy($image_1));
+imagegif($image_2,'./gen.png');
+
+
+
+
+
+
+function imageresize($filename,$newwidth,$newheight){
+
+    if(!empty($filename) && file_exists($filename)){
+        list($width, $height) = getimagesize($filename);
+        $thumb = imagecreatetruecolor($newwidth, $newheight);
+
+        $suffix = strrchr($filename,'.');
+        switch($suffix){
+            case '.gif':
+                $source = imagecreatefromgif($filename);
+                imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+                break;
+            case '.png':
+                $source = imagecreatefrompng($filename);
+                imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+                break;
+            case '.jpg':
+                $source = imagecreatefromjpeg($filename);
+                imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+                break;
+            case '.bmp':
+                $source = imagecreatefromwbmp($filename);
+                imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+                break;
+        }
+        return $thumb;
+    }else {
+        die('INVALID IMAGE');
+    }
+}
+?>
