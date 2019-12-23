8a929ba9212129eaa5d9b35f503ebe07dda771de
Jiang <1873645345@qq.com>
2019-12-23 11:24:44 +0800

    live2d

--- phpDemo/regex/rule.php
+++ phpDemo/regex/rule.php
@@ -10,4 +10,10 @@ if(preg_match($rule, 12345678)) {
 	echo "不匹配";
 }
 
+
+//替换掉字符串中的中文
+
+// $str = "h把t这t行p字s里:的/中/文p都a删n掉.其b它a的i都d留u下.把c这o行m字/里s的/中1文S都C删r掉2其T它u的J都T留K下d把Q这v行6字Z里w的O中u文A都9删Q掉c其Q";
+// $result = preg_match_all("/[^\x{4e00}-\x{9fa5}]+/u",$str,$match);
+
 ?>
