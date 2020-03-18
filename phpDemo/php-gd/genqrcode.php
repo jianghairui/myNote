6d61a610cf0c8a59f6e8e36123c9a251250c1aab
Jiang <1873645345@qq.com>
2020-03-18 16:14:21 +0800

    微信登录授权

--- phpDemo/php-gd/genqrcode.php
+++ phpDemo/php-gd/genqrcode.php
@@ -1,9 +1,14 @@
 <?php
+
+
 //生成二维码的DEMO
 include "../phpqrcode/phpqrcode.php";
 $value="http://www.baidu.com";
 $errorCorrectionLevel = "L"; // 纠错级别：L、M、Q、H
 $matrixPointSize = "4"; // 点的大小：1到10
-$path = './qrcode.png';
-QRcode::png($value, false, $errorCorrectionLevel, $matrixPointSize);
+$path = './123456.png';
+QRcode::png($value, $path, $errorCorrectionLevel, $matrixPointSize,4);
+
+
+
 ?>
--- phpDemo/phpqrcode/phpqrcode.php
+++ phpDemo/phpqrcode/phpqrcode.php
@@ -3088,10 +3088,10 @@
         }
         
         //----------------------------------------------------------------------
-        public static function png($text, $outfile = false, $level = QR_ECLEVEL_L, $size = 3, $margin = 4, $saveandprint=false) 
+        public static function png($text, $outfile = false, $level = QR_ECLEVEL_L, $size = 3, $margin = 4, $saveandprint=true)
         {
             $enc = QRencode::factory($level, $size, $margin);
-            return $enc->encodePNG($text, $outfile, $saveandprint=false);
+            return $enc->encodePNG($text, $outfile, $saveandprint);
         }
 
         //----------------------------------------------------------------------
